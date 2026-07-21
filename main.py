12e3f6144d93973af2dc7e7963f3d31799342c6d
xcabreira <joabfla15@gmail.com>
2026-07-21 12:43:42 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+nomeProduto = input("Insira o nome do produto: ")
+unidadeProduto = input("Tipo de unidade: ")
+valorProduto = float(input("Valor unitário: "))
+qtdEstoque = float(input("Estoque disponivel: "))
+
