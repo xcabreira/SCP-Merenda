151ee74802ac98fcf154b3570bec973590cf05f4
Joab Cabreira <joabfla15@gmail.com>
2026-07-22 16:43:28 -0400

    version 0 ok, nstep

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 # Inicio
 print("--Sistema de Cadastro de Produtos--")
 opcaoIniciar = input("1 - Cadastrar produto \n 2 - Buscar produto \n 3 - Editar produtos \n 4 - Excluir produto \n 5 - Sair \n")
@@ -23,7 +24,7 @@ if opcaoIniciar == "1":
 #--------------------------
 
 #Buscar Pordutos
-elif opcaoIniciar == "2":
+if opcaoIniciar == "2":
     buscarProduto = input("Qual produto deseja buscar?: ")
 
     if buscarProduto == nomeProduto:
@@ -35,30 +36,39 @@ elif opcaoIniciar == "2":
 #--------------------------
 
 #Alterar valores do produto
-elif opcaoIniciar == "3":
+if opcaoIniciar == "3":
     alterarProduto = input("Qual produto deseja alterar? insira o nome do produto: ")
-    alterarEscolha = input("O que deseja alterar?: \n Valor do Produto - 1 \n Quantidade no estoque - 2 \n Nome do Produto - 3 \n")
-    match alterarEscolha:
-        case "1":
-            novoValor = float(input("Qual será o novo valor?: "))
-            produtos[tipoProduto][nomeProduto]["valor"] = novoValor
-            print("Valor Alterado")
-        case "2":
-            novaQtd = float(input("Nova quantidade em estoque: "))
-            produtos[tipoProduto][nomeProduto]["estoque"] = novaQtd
-            print("Estoque Alterado")
-        case "3":
-            novoNome = input("Qual o nome do produto: ")
-            produtos[tipoProduto][nomeProduto] = novoNome
-            print("Nome Alterado") 
-        case _:
-            print("Alteração não realizada!")
+    if alterarProduto == nomeProduto:
+        alterarEscolha = input("O que deseja alterar?: \n Valor do Produto - 1 \n Quantidade no estoque - 2 \n Nome do Produto - 3 \n")
+        match alterarEscolha:
+            case "1":
+                novoValor = float(input("Qual será o novo valor?: "))
+                produtos[tipoProduto][nomeProduto]["valor"] = novoValor
+                print("Valor Alterado")
+            case "2":
+                novaQtd = float(input("Nova quantidade em estoque: "))
+                produtos[tipoProduto][nomeProduto]["estoque"] = novaQtd
+                print("Estoque Alterado")
+            case "3":
+                novoNome = input("Qual o nome do produto: ")
+                produtos[tipoProduto][nomeProduto] = novoNome
+                print("Nome Alterado") 
+            case _:
+                print("Alteração não realizada!")
+    else: 
+        print("Produto não encontrado!")
 #--------------------
 
 #Excluir produto
-removerProduto = input("Qual produto deseja remover? ")
+if opcaoIniciar == "4":
+    removerProduto = input("Qual produto deseja remover? ")
 
-if removerProduto == produtos['frutas']['nome']:
-    del produtos['frutas']
-    print(produtos)
+    if removerProduto == nomeProduto:
+        del produtos[tipoProduto][nomeProduto]
+        print(f"Produto {nomeProduto} removido")
+if opcaoIniciar == "5":
+    print("saindo...")
+    time.sleep(1.5)
+    print("Até a próxima!")
+    
     
