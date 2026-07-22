58b292d8d3b7817a73d3c9bf0e73220f424a48db
xcabreira <joabfla15@gmail.com>
2026-07-22 12:49:22 -0400

    added options for choice in terminal

--- main.py
+++ main.py
@@ -1,58 +1,64 @@
-# Cadastrar Produto
+# Inicio
 print("--Sistema de Cadastro de Produtos--")
-nomeProduto = input("Insira o nome do produto: ")
-unidadeProduto = input("Tipo de unidade: ")
-valorProduto = float(input("Valor unitário: "))
-qtdEstoque = float(input("Estoque disponivel: "))
+opcaoIniciar = input("1 - Cadastrar produto \n 2 - Buscar produto \n 3 - Editar produtos \n 4 - Excluir produto \n 5 - Sair \n")
 
-produtos = {
-    "frutas": {
-        "nome": nomeProduto,
-        "unidade": unidadeProduto,
-        "valor": valorProduto,
-        "estoque": qtdEstoque
+# Cadastrar Produto
+if opcaoIniciar == "1":
+    tipoProduto = input("Qual o tipo de produto? Ex.: Frutas, Carnes, Bebidas, etc...: ")
+    nomeProduto = input("Insira o nome do produto: ")
+    valorProduto = float(input("Valor unitário: "))
+    qtdEstoque = float(input("Estoque disponivel: "))
+
+    produtos = {
+        tipoProduto: {
+            nomeProduto: {
+                "valor": valorProduto,
+                "estoque": qtdEstoque
+            }
+        }
     }
-}
 
-print("Produto Cadastrado!")
+    print("Produto Cadastrado! \n")
 
 #--------------------------
 
 #Buscar Pordutos
-buscarProduto = input("Qual produto deseja buscar?: ")
+elif opcaoIniciar == "2":
+    buscarProduto = input("Qual produto deseja buscar?: ")
 
-if buscarProduto == nomeProduto:
-    for produto in produtos.values():
-        print(produto)
-else: 
-    print("Produto não encontrado ou não cadastrado!")
+    if buscarProduto == nomeProduto:
+        for produto in produtos[tipoProduto][nomeProduto].values():
+            print(produto)
+    else: 
+        print("Produto não encontrado ou não cadastrado!")
 
 #--------------------------
 
 #Alterar valores do produto
-alterarProduto = input("O que deseja alterar?: \n Valor do Produto - 1 \n Quantidade no estoque - 2 \n Nome do Produto - 3 \n Unidade de pesagem - 4 \n")
-match alterarProduto:
-    case "1":
-        novoValor = float(input("Qual será o novo valor?: "))
-        produtos["valor"] = novoValor
-        print("Valor Alterado")
-    case "2":
-        novaQtd = float(input("Nova quantidade em estoque: "))
-        produtos["estoque"] = novaQtd
-        print("Estoque Alterado")
-    case "3":
-        novoNome = input("Qual o nome do produto: ")
-        produtos["nome"] = novoNome
-        print("Nome Alterado")
-    case "4":
-        novaUnidade = input("Qual unidade de pesagem?: ")
-        produtos["unidade"] = novaUnidade
-        print("Unidade Alterada")
-    case _:
-        print("Alteração não realizada!")
+elif opcaoIniciar == "3":
+    alterarProduto = input("Qual produto deseja alterar? insira o nome do produto: ")
+    alterarEscolha = input("O que deseja alterar?: \n Valor do Produto - 1 \n Quantidade no estoque - 2 \n Nome do Produto - 3 \n")
+    match alterarEscolha:
+        case "1":
+            novoValor = float(input("Qual será o novo valor?: "))
+            produtos[tipoProduto][nomeProduto]["valor"] = novoValor
+            print("Valor Alterado")
+        case "2":
+            novaQtd = float(input("Nova quantidade em estoque: "))
+            produtos[tipoProduto][nomeProduto]["estoque"] = novaQtd
+            print("Estoque Alterado")
+        case "3":
+            novoNome = input("Qual o nome do produto: ")
+            produtos[tipoProduto][nomeProduto] = novoNome
+            print("Nome Alterado") 
+        case _:
+            print("Alteração não realizada!")
 #--------------------
 
 #Excluir produto
 removerProduto = input("Qual produto deseja remover? ")
 
-if removerProduto == 
+if removerProduto == produtos['frutas']['nome']:
+    del produtos['frutas']
+    print(produtos)
+    
