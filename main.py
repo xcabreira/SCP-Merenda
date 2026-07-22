b180e9ef40214f10eff50f54f59c683c34dbce5d
xcabreira <joabfla15@gmail.com>
2026-07-22 12:11:19 -0400

    att main.py

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ valorProduto = float(input("Valor unitário: "))
 qtdEstoque = float(input("Estoque disponivel: "))
 
 produtos = {
-    "nome": nomeProduto,
-    "unidade": unidadeProduto,
-    "valor": valorProduto,
-    "estoque": qtdEstoque
+    "frutas": {
+        "nome": nomeProduto,
+        "unidade": unidadeProduto,
+        "valor": valorProduto,
+        "estoque": qtdEstoque
+    }
 }
 
 print("Produto Cadastrado!")
@@ -28,23 +30,29 @@ else:
 #--------------------------
 
 #Alterar valores do produto
-alterarProduto = input("Deseja alterar o produto?: y/n")
-if alterarProduto == "y":
-    alteracao = input("O que deseja alterar?: \n Valor do Produto - 1 \n Quantidade no estoque - 2 \n Nome do Produto - 3 \n Unidade de pesagem - 4")
-    match alteracao:
-        case 1:
-            novoValor = float(input("Qual será o novo valor?: "))
-            produtos["valor"] = novoValor
-        case 2:
-            novaQtd = float(input("Nova quantidade em estoque: "))
-            produtos["estoque"] = novaQtd
-        case 3:
-            novoNome = input("Qual o nome do produto: ")
-            produtos["nome"] = novoNome
-        case 4:
-            novaUnidade = input("Qual unidade de pesagem?: ")
-            produtos["unidade"] = novaUnidade
-            
+alterarProduto = input("O que deseja alterar?: \n Valor do Produto - 1 \n Quantidade no estoque - 2 \n Nome do Produto - 3 \n Unidade de pesagem - 4 \n")
+match alterarProduto:
+    case "1":
+        novoValor = float(input("Qual será o novo valor?: "))
+        produtos["valor"] = novoValor
+        print("Valor Alterado")
+    case "2":
+        novaQtd = float(input("Nova quantidade em estoque: "))
+        produtos["estoque"] = novaQtd
+        print("Estoque Alterado")
+    case "3":
+        novoNome = input("Qual o nome do produto: ")
+        produtos["nome"] = novoNome
+        print("Nome Alterado")
+    case "4":
+        novaUnidade = input("Qual unidade de pesagem?: ")
+        produtos["unidade"] = novaUnidade
+        print("Unidade Alterada")
+    case _:
+        print("Alteração não realizada!")
 #--------------------
 
 #Excluir produto
+removerProduto = input("Qual produto deseja remover? ")
+
+if removerProduto == 
